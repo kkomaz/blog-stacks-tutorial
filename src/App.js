af53c48e9b655496b1a5d41d25598c45dff192e1
Alex Lee <alex@uniteus.com>
2019-01-04 07:24:23 -0500

    Lesson-3: App Creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/main.scss';
+import { Button } from 'react-bulma-components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Button color="primary">
+          This is a bulma component button
+        </Button>
       </div>
     );
   }
