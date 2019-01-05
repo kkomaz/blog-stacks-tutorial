bc8954002ab4a19097da21a65fb43537404371c6
Alex Lee <alex@uniteus.com>
2019-01-05 15:06:04 -0500

    Lesson-4: Login Part I

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import './stylesheets/main.scss';
+import 'stylesheets/main.scss';
 import { Button } from 'react-bulma-components'
+import { appConfig } from 'utils/constants'
+import { UserSession } from 'blockstack'
 
 class App extends Component {
+  state = {
+    userSession: new UserSession({ appConfig })
+  }
+
+  handleSignIn = () => {
+    const { userSession } = this.state
+    userSession.redirectToSignIn()
+  }
+
+  handleSignOut = () => {
+    console.log('handle sign out')
+  }
+
   render() {
+    const { userSession } = this.state
+
     return (
       <div className="App">
-        <Button color="primary">
-          This is a bulma component button
-        </Button>
+        {
+          userSession.isUserSignedIn() ?
+          <Button color="primary" onClick={this.handleSignOut}>
+            Sign Out
+          </Button> :
+          <Button color="primary" onClick={this.handleSignIn}>
+            Sign In
+          </Button>
+        }
       </div>
     );
   }
